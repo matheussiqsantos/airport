0e8ddc59d862d5ba1a8f5c00bb430c8591f3137d
Matheus Siqueira <matheus.siqsantos05@gmail.com>
2025-11-27 14:52:25 -0300

    Término da Fase 3 - Implementando endpoint FindByCityIgnoreCase. - Update AirportRepository - Adicionado assinatura de método findByCityIgnoreCase;
    - Update AirportService - Adicionado método findByCity
    - Update AirportController - Adicionado endpoint /city/{cityName}

--- src/main/java/br/dev/matheus/airports/controllers/AirportController.java
+++ src/main/java/br/dev/matheus/airports/controllers/AirportController.java
@@ -4,7 +4,9 @@ import br.dev.matheus.airports.entities.Airport;
 import br.dev.matheus.airports.service.AirportService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,6 +27,19 @@ public class AirportController
         
         List<Airport> result = airportService.findAll();
         return result;
+    }
+    
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName) {
+        List<Airport> result = airportService.findByCity(cityName);
+        
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+            
+        } else {
+            return ResponseEntity.ok(result);
+        }
+        
     }
     
 }
--- src/main/java/br/dev/matheus/airports/repositories/AirportRepository.java
+++ src/main/java/br/dev/matheus/airports/repositories/AirportRepository.java
@@ -1,6 +1,7 @@
 package br.dev.matheus.airports.repositories;
 
 import br.dev.matheus.airports.entities.Airport;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -8,5 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author DIT2B
  */
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+    
+    List<Airport> findByCityIgnoreCase(String city);
     
 }
